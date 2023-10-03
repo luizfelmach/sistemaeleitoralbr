3d291400dd754dfc7b719f9560631c2eb8665cc2
Luiz Felipe Machado <56140722+luizfelmach@users.noreply.github.com>
2023-10-03 15:24:37 -0300

    add model candidate

---
+++ src/domain/model/Candidate.java
@@ -0,0 +1,13 @@
+package domain.model;
+
+public class Candidate {
+    private Boolean isElected;
+
+    public Boolean isElected() {
+        return isElected;
+    }
+
+    public void setIsElected(Boolean isElected) {
+        this.isElected = true;
+    }
+}
