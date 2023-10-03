b8b1c130f4691f9595bf6ae47fea75a2b42fbd9a
Luiz Felipe Machado <56140722+luizfelmach@users.noreply.github.com>
2023-10-03 15:24:54 -0300

    add usecases

---
+++ src/domain/usecases/ElectedCandidates.java
@@ -0,0 +1,22 @@
+package domain.usecases;
+
+import domain.model.Candidate;
+import repository.CandidateRepository;
+
+public class ElectedCandidates {
+    CandidateRepository candidateRepository;
+
+    ElectedCandidates(CandidateRepository candidateRepository) {
+        this.candidateRepository = candidateRepository;
+    }
+
+    public int execute() {
+        int totalElected = 0;
+        for (Candidate candidate : candidateRepository.getAllCandidates()) {
+            if (candidate.isElected()) {
+                totalElected += 1;
+            }
+        }
+        return totalElected;
+    }
+}
