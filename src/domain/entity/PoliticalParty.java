928e7615fc6ea91c20fa70ba1b45d77a84cd1a3f
Luiz Felipe Machado <56140722+luizfelmach@users.noreply.github.com>
2023-10-05 11:05:14 -0300

    add entity political party

---
+++ src/domain/entity/PoliticalParty.java
@@ -0,0 +1,47 @@
+package domain.entity;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class PoliticalParty {
+    private String name;
+    private int rollCallVotes;
+    private int captionVotes;
+    Set<Candidate> candidates = new HashSet<>();
+
+    public PoliticalParty(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getRollCallVotes() {
+        return rollCallVotes;
+    }
+
+    public int getCaptionVotes() {
+        return captionVotes;
+    }
+
+    public int getTotalVotes() {
+        return getRollCallVotes() + getCaptionVotes();
+    }
+
+    public Set<Candidate> getCandidates() {
+        return new HashSet<>(candidates);
+    }
+
+    public void addCandidate(Candidate candidate) {
+        candidates.add(candidate);
+    }
+
+    public void addRollCallVote(int partial) {
+        rollCallVotes += partial;
+    }
+
+    public void addCaptionVote(int partial) {
+        rollCallVotes += partial;
+    }
+}
