b545724d63220b69d16deaf83948cd41476870dc
Luiz Felipe Machado <56140722+luizfelmach@users.noreply.github.com>
2023-10-05 07:56:38 -0300

    add implementation in memory candidate repository

--- src/adapters/InMemoryCandidateRepository.java
+++ src/adapters/InMemoryCandidateRepository.java
@@ -1,6 +1,8 @@
 package adapters;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -9,23 +11,41 @@ import domain.entity.Candidate;
 import domain.repository.CandidateRepository;
 
 public class InMemoryCandidateRepository implements CandidateRepository {
+    private Set<Candidate> candidates;
+
+    public InMemoryCandidateRepository() {
+    }
+
     @Override
     public Set<Candidate> getAllCandidates() {
-        return new HashSet<>();
+        return new HashSet<>(candidates);
     }
 
     @Override
     public Set<Candidate> getElectedCandidates() {
-        return new HashSet<>();
+        Set<Candidate> electedCandidates = new HashSet<>();
+        for (Candidate candidate : candidates) {
+            if (candidate.isElected()) {
+                electedCandidates.add(candidate);
+            }
+        }
+        return electedCandidates;
     }
 
     @Override
     public int getNumberOfElectedCandidates() {
-        return 0;
+        return getElectedCandidates().size();
     }
 
     @Override
     public List<Candidate> getMostVotedCandidates() {
-        return new ArrayList<>();
+        List<Candidate> allCandidates = new ArrayList<>(candidates);
+        Collections.sort(allCandidates, new Comparator<Candidate>() {
+            @Override
+            public int compare(Candidate c1, Candidate c2) {
+                return c1.getTotalVotes() - c2.getTotalVotes();
+            }
+        });
+        return allCandidates;
     }
 }
