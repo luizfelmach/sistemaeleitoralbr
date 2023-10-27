8c3263584c1b5bcceb8a8fa0796f8a67643e4ec7
Luiz Felipe Machado <56140722+luizfelmach@users.noreply.github.com>
2023-10-27 13:50:24 -0300

    rename report 4

--- src/domain/usecases/CandidatesWouldBeElectedInMajority.java
+++ src/domain/usecases/CandidatesWouldBeElectedInMajority.java
@@ -1,32 +1,34 @@
 package domain.usecases;
 
+import domain.entity.Candidate;
+import domain.repository.ElectionRepository;
+import domain.util.ComparatorUseCase;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-import domain.entity.Candidate;
-import domain.repository.ElectionRepository;
-import domain.util.ComparatorUseCase;
+public class CandidatesWouldBeElectedInMajority {
 
-public class ElectedCandidatesIfWasMajorityUseCase {
-    ElectionRepository electionRepository;
+  ElectionRepository electionRepository;
 
-    public ElectedCandidatesIfWasMajorityUseCase(ElectionRepository electionRepository) {
-        this.electionRepository = electionRepository;
+  public CandidatesWouldBeElectedInMajority(
+    ElectionRepository electionRepository
+  ) {
+    this.electionRepository = electionRepository;
+  }
+
+  public List<Candidate> execute() {
+    List<Candidate> candidates = new ArrayList<>();
+    List<Candidate> mostVotedCandidates = electionRepository.getAllCandidates();
+    Collections.sort(mostVotedCandidates, ComparatorUseCase.candidateMostVoted);
+    int numberOfElectedCandidates = electionRepository.getNumberOfElectedCandidates();
+    for (int i = 0; i < numberOfElectedCandidates; i++) {
+      Candidate candidate = mostVotedCandidates.get(i);
+      candidate.setRankingMostVoted(i + 1);
+      if (!candidate.isElected()) {
+        candidates.add(candidate);
+      }
     }
-
-    public List<Candidate> execute() {
-        List<Candidate> candidates = new ArrayList<>();
-        List<Candidate> mostVotedCandidates = electionRepository.getAllCandidates();
-        Collections.sort(mostVotedCandidates, ComparatorUseCase.candidateMostVoted);
-        int numberOfElectedCandidates = electionRepository.getNumberOfElectedCandidates();
-        for (int i = 0; i < numberOfElectedCandidates; i++) {
-            Candidate candidate = mostVotedCandidates.get(i);
-            candidate.setRankingMostVoted(i + 1);
-            if (!candidate.isElected()) {
-                candidates.add(candidate);
-            }
-        }
-        return candidates;
-    }
+    return candidates;
+  }
 }
